62479223c7ef6e4e85bd35c0b34ca04d5dadf1b7
Freddy Michelena <freddy@michelena.com>
2021-07-22 18:57:59 -0400

    Creación de SharedModule y los componentes del Sidebar y Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StravaModule } from './strava/strava.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		StravaModule
+		StravaModule,
+		SharedModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
